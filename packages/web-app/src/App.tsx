cafec246b8dc948cef78c6e61f503dc6c7e136c2
igdev <dorultanianos@gmail.com>
2025-06-07 10:23:32 +0300

    feat: integrate Tailwind CSS and simplify App component

--- packages/web-app/src/App.tsx
+++ packages/web-app/src/App.tsx
@@ -1,51 +1,9 @@
-import {Connector, useAccount, useConnect, useDisconnect} from 'wagmi';
 
 function App() {
-  const account = useAccount()
-  const { connectors, connectAsync, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
-  const handleConnect = (connector: Connector) => {
-    return async () => {
-      const c = await connectAsync({connector});
-      console.log(c);
-    }
-  }
 
   return (
     <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={handleConnect(connector)}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
+      <h1>Hello</h1>
     </>
   )
 }
