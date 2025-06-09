4c744567165df53772252296c4c332c89bfa9041
igdev <dorultanianos@gmail.com>
2025-06-09 15:57:20 +0300

    feat: update title and navbar heading for branding consistency

--- packages/web-app/src/components/navbar/index.tsx
+++ packages/web-app/src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import {ConnectButton} from '@rainbow-me/rainbowkit';
 export default function Navbar() {
   return (
       <div className="container mx-auto flex justify-between py-2 items-center">
-        <h1 className="text-2xl font-bold">🐝 BeeFunded</h1>
+        <h1 className="text-2xl font-bold">🐝 Funded</h1>
         <ConnectButton/>
       </div>
   )
