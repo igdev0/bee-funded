3595c4c477778f92efce57a64764bd3eb4eb1697
igdev <dorultanianos@gmail.com>
2025-06-20 09:01:35 +0300

    feat: implement donation pool CRU operations with TypeORM integration

--- packages/backend/src/donation-pool/donation-pool.service.ts
+++ packages/backend/src/donation-pool/donation-pool.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDonationPoolDto } from './dto/create-donation-pool.dto';
 import { UpdateDonationPoolDto } from './dto/update-donation-pool.dto';
+import { Repository } from 'typeorm';
+import { DonationPool } from './entities/donation-pool.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class DonationPoolService {
+  constructor(
+    @InjectRepository(DonationPool)
+    private donationPoolRepository: Repository<DonationPool>,
+  ) {}
+
   create(createDonationPoolDto: CreateDonationPoolDto) {
-    return 'This action adds a new donationPool';
+    const pool = this.donationPoolRepository.create(createDonationPoolDto);
+    return this.donationPoolRepository.save(pool);
   }
 
   findAll() {
-    return `This action returns all donationPool`;
+    return this.donationPoolRepository.find();
+  }
+
+  findAllByUserId(userId: string) {
+    return this.donationPoolRepository.find({
+      where: {
+        user: userId,
+      },
+    });
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} donationPool`;
+    return this.donationPoolRepository.findOneBy({
+      id,
+    });
   }
 
   update(id: string, updateDonationPoolDto: UpdateDonationPoolDto) {
-    return `This action updates a #${id} donationPool`;
+    return this.donationPoolRepository.update({ id }, updateDonationPoolDto);
   }
 }
