cfa1c83e05709db8bfa79b1a29186a2d1b31d4db
igdev <dorultanianos@gmail.com>
2025-06-22 17:22:28 +0300

    feat: update PlatformScreen to allow unauthenticated access

--- packages/web-app/src/screens/platform/index.tsx
+++ packages/web-app/src/screens/platform/index.tsx
@@ -4,7 +4,7 @@ import useAppStore from '@/stores/app.ts';
 export default function PlatformScreen() {
   const user = useAppStore().user;
   return (
-      <Screen requiresAuthRedirectTo="/">
+      <Screen requiresAuth={false}>
         <h1>Hello {user?.username}</h1>
       </Screen>
   );
