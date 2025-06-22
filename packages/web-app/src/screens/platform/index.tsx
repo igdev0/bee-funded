5ffeb1d4e247a08677436250a7d85054c9deb9cc
igdev <dorultanianos@gmail.com>
2025-06-22 17:41:46 +0300

    feat: add Avatar component and integrate it into PlatformScreen with styling

--- packages/web-app/src/screens/platform/index.tsx
+++ packages/web-app/src/screens/platform/index.tsx
@@ -1,11 +1,19 @@
 import Screen from '@/components/screen';
 import useAppStore from '@/stores/app.ts';
+import "./styles.css";
+import Avatar from '@/components/avatar';
 
+const DEMO_IMAGE = "/public/male.png"
 export default function PlatformScreen() {
   const user = useAppStore().user;
   return (
-      <Screen requiresAuth={false}>
-        <h1>Hello {user?.username}</h1>
+      <Screen requiresAuth={false} className="page">
+        <div className="banner">
+        </div>
+        <div className="avatar-wrapper">
+          <Avatar imageUrl={DEMO_IMAGE}/>
+          @{user?.username}
+        </div>
       </Screen>
   );
 }
