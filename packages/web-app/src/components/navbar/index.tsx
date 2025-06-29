d6e2e4c8eb01e77b92489c73c901ca00a9d46537
igdev <dorultanianos@gmail.com>
2025-06-29 22:53:35 +0300

    feat: replace text logo with image logo in Navbar component

--- packages/web-app/src/components/navbar/index.tsx
+++ packages/web-app/src/components/navbar/index.tsx
@@ -4,7 +4,7 @@ import WalletConnectButton from '@/components/wallet-connect-button';
 export default function Navbar() {
   return (
       <div className="container mx-auto flex justify-between py-2 items-center">
-        <Link to="/" className="text-2xl font-bold">🐝 Funded</Link>
+        <Link to="/" className="text-2xl font-bold"><img src="/beefunded-logo-buzz.png" width={150} alt="logo"/> </Link>
         <WalletConnectButton/>
       </div>
   );
