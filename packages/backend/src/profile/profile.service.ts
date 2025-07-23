eaa04a30037e884081ea8274f407e1477ab61f39
igdev <dorultanianos@gmail.com>
2025-07-23 20:05:45 +0300

    feat(profile): import database into the module and create the update method on the ProfileService class

--- packages/backend/src/profile/profile.service.ts
+++ packages/backend/src/profile/profile.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import ProfileEntity from './entities/profile.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  logger = new Logger('ProfileService');
+
+  constructor(
+    @InjectRepository(ProfileEntity)
+    private readonly profileRepository: Repository<ProfileEntity>,
+  ) {}
+
+  async update(profileId: string, updateProfileDto: UpdateProfileDto) {
+    await this.profileRepository.update(profileId, updateProfileDto);
+    return this.profileRepository.findOne({ where: { id: profileId } });
+  }
+}
