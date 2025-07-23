52193b92ba621fde705db8b5376fab3238e2e18c
igdev <dorultanianos@gmail.com>
2025-07-23 21:29:43 +0300

    feat(profile): add update endpoint

--- packages/backend/src/profile/profile.controller.ts
+++ packages/backend/src/profile/profile.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
 import { ProfileService } from './profile.service';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { GetUser } from '../user/user.decorator';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  @Patch()
+  @UseGuards(AuthGuard)
+  update(
+    @GetUser() user: UserEntity,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    return this.profileService.update(user.profile.id, updateProfileDto);
+  }
 }
