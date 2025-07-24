477ce19d83b2f90d7acdc28a38660abb62bfbdfe
igdev <dorultanianos@gmail.com>
2025-07-24 15:28:05 +0300

    feat(profile): add multer storage config

--- packages/backend/src/profile/profile.module.ts
+++ packages/backend/src/profile/profile.module.ts
@@ -6,13 +6,23 @@ import ProfileEntity from './entities/profile.entity';
 import { AuthModule } from '../auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { MulterModule } from '@nestjs/platform-express';
-import * as process from 'node:process';
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
 @Module({
   imports: [
     AuthModule,
     MulterModule.register({
-      dest: process.cwd() + 'uploads/',
+      storage,
     }),
     CacheModule.register(),
     TypeOrmModule.forFeature([ProfileEntity]),
