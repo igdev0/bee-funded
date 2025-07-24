14a893994cd69b5f9edc1d4ae3c6511bcd181647
igdev <dorultanianos@gmail.com>
2025-07-24 15:08:13 +0300

    test(profile): add test case for profileService.update

--- packages/backend/src/profile/profile.service.spec.ts
+++ packages/backend/src/profile/profile.service.spec.ts
@@ -1,21 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileService } from './profile.service';
-import { ModuleMocker } from 'jest-mock';
+import { Mocked, TestBed } from '@suites/unit';
+import { Repository } from 'typeorm';
+import ProfileEntity from './entities/profile.entity';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 describe('ProfileService', () => {
   let service: ProfileService;
-
+  let profileRepository: Mocked<Repository<ProfileEntity>>;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfileService],
-    })
-      .useMocker(() => new ModuleMocker(global))
-      .compile();
-
-    service = module.get<ProfileService>(ProfileService);
+    const { unit, unitRef } = await TestBed.solitary(ProfileService).compile();
+    service = unit;
+    profileRepository = unitRef.get('ProfileEntityRepository');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should update profile database and return the new ProfileEntity', async () => {
+    const profileId = 'some-uuid';
+    const update: UpdateProfileDto = {
+      bio: 'Some bio',
+      displayName: 'Some displayName',
+      username: 'userName',
+      email: 'userEmail',
+      socialLinks: ['https://linkedin.com/in/some-uuid'],
+    };
+    await service.update(profileId, update);
+
+    expect(profileRepository.update).toHaveBeenCalledWith(profileId, update);
+    expect(profileRepository.findOne).toHaveBeenCalledWith({
+      where: { id: profileId },
+    });
   });
 });
