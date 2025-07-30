2118f21b22ab63fd48cdbe2c756fd5198af954c7
igdev <dorultanianos@gmail.com>
2025-07-30 15:38:34 +0300

    refactor(MockERC731): update burn authorization check

--- packages/hardhat/contracts/mocks/MockERC721.sol
+++ packages/hardhat/contracts/mocks/MockERC721.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.30;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {Ownable} from "hardhat-deploy/solc_0.8/openzeppelin/access/Ownable.sol";
 
 contract MockPromiseNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
 
-    constructor() ERC721("MockPromiseNFT", "MPN") {}
+    constructor() ERC721("MockPromiseNFT", "MPN") Ownable(msg.sender) {
+    }
 
     function mint(address to, string calldata uri) external onlyOwner returns (uint256) {
         uint256 newTokenId = ++_tokenIdCounter;
@@ -19,7 +19,7 @@ contract MockPromiseNFT is ERC721URIStorage, Ownable {
     }
 
     function burn(uint256 tokenId) external {
-        require(_isAuthorized(_owner, _msgSender(), tokenId), "Not authorized");
+        _checkAuthorized(_ownerOf(tokenId), _msgSender(), tokenId);
         _burn(tokenId);
     }
 
