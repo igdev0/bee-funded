47bdcaf9c63776378c61ed71028b8620df076460
igdev <dorultanianos@gmail.com>
2025-08-06 22:48:29 +0300

    feat(DonationPool): add moduleInit fn

--- packages/backend/src/donation-pool/donation-pool.module.ts
+++ packages/backend/src/donation-pool/donation-pool.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DonationPoolService } from './donation-pool.service';
 import { DonationPoolController } from './donation-pool.controller';
+import { ConfigModule } from '@nestjs/config';
+import ContractsConfig from '../contracts.config';
 
 @Module({
+  imports: [ConfigModule.forFeature(ContractsConfig)],
   providers: [DonationPoolService],
   controllers: [DonationPoolController],
 })
--- packages/backend/src/donation-pool/donation-pool.service.ts
+++ packages/backend/src/donation-pool/donation-pool.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ChainConfig } from '../contracts.config';
+import { Contract, WebSocketProvider } from 'ethers';
 
 @Injectable()
-export class DonationPoolService {}
+export class DonationPoolService implements OnModuleInit {
+  constructor(private readonly config: ConfigService) {}
+
+  async onModuleInit(): Promise<void> {
+    const chains = this.config.get<ChainConfig[]>('contracts');
+    if (!chains) {
+      throw new Error('Contracts config must be set');
+    }
+
+    for (const chain of chains) {
+      const provider = new WebSocketProvider(chain.rpcUrl);
+
+      const { abi: beeFundedCoreAbi, address: beeFundedCoreAddress } =
+        chain.contracts.BeeFundedCore;
+
+      const contract = new Contract(
+        beeFundedCoreAddress,
+        beeFundedCoreAbi,
+        provider,
+      );
+
+      await contract.on('DonationPoolCreated', () => {
+        console.log('DonationPoolCreated');
+      });
+    }
+  }
+}
