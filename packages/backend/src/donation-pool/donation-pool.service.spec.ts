1987310998f5010b69ccdc0daa7b4aa07d3cec69
igdev <dorultanianos@gmail.com>
2025-08-08 10:38:37 +0300

    test(donation-pool): add test cases for creating a donation pool

--- packages/backend/src/donation-pool/donation-pool.service.spec.ts
+++ packages/backend/src/donation-pool/donation-pool.service.spec.ts
@@ -1,12 +1,15 @@
 import { DonationPoolService } from './donation-pool.service';
-import { TestBed, Mocked } from '@suites/unit';
+import { Mocked, TestBed } from '@suites/unit';
 import contractsConfig from '../contracts.config';
 import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+import { DonationPoolEntity } from './entities/donation-pool.entity';
+import CreateDonationPoolDto from './dto/create-donation-pool.dto';
 
 describe('DonationPoolService', () => {
   let service: DonationPoolService;
   let configService: Mocked<ConfigService>;
-
+  let donationPoolRepository: Mocked<Repository<DonationPoolEntity>>;
   beforeEach(async () => {
     const { unitRef, unit } =
       await TestBed.solitary(DonationPoolService).compile();
@@ -14,8 +17,38 @@ describe('DonationPoolService', () => {
     service = unit;
     configService = unitRef.get(ConfigService);
     configService.get.mockReturnValue(contractsConfig());
+    donationPoolRepository = unitRef.get('DonationPoolEntityRepository');
   });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('creating a donation pool', () => {
+    it('should be able to create a main donation pool', async () => {
+      await service.create({ kind: 'main' });
+
+      expect(donationPoolRepository.create).toHaveBeenCalledWith({
+        kind: 'main',
+        status: 'publishing',
+      });
+    });
+
+    it('should be able to create a donation pool for a specific objective', async () => {
+      const data: CreateDonationPoolDto = {
+        kind: 'objective',
+        cap: 100000,
+        image: 'https://somewhere.com',
+        tags: ['tag'],
+        description: 'Some description',
+        title: 'Some title',
+        valuation_token: '0x00000000000',
+      };
+      await service.create(data);
+
+      expect(donationPoolRepository.create).toHaveBeenCalledWith({
+        ...data,
+        status: 'publishing',
+      });
+    });
   });
 });
