6e14259ffb440f76e9f95fca74e50d2744aab1d7
igdev <dorultanianos@gmail.com>
2025-08-10 11:12:57 +0300

    refactor(database): clean up the return types from config

--- packages/backend/src/database/database.module.ts
+++ packages/backend/src/database/database.module.ts
@@ -14,13 +14,13 @@ import { DonationPoolEntity } from '../donation-pool/entities/donation-pool.enti
       imports: [ConfigModule.forFeature(databaseConfig)],
       inject: [ConfigService],
       useFactory(config: ConfigService) {
-        const type = config.get<DatabaseType>('db.type');
-        const host: string = config.get('db.host') ?? 'localhost';
-        const port: number = config.get('db.port') ?? 5432;
-        const database: string = config.get('db.database') ?? 'yourdb';
-        const username: string = config.get('db.username') ?? 'admin';
-        const password: string = config.get('db.password') ?? 'admin';
-        const synchronize: boolean = config.get('db.sync') ?? false;
+        const type = config.get<DatabaseType>('db.type') as DatabaseType;
+        const host = config.get<string>('db.host') ?? 'localhost';
+        const port = config.get<number>('db.port') ?? 5432;
+        const database = config.get<string>('db.database') ?? 'yourdb';
+        const username = config.get<string>('db.username') ?? 'admin';
+        const password = config.get<string>('db.password') ?? 'admin';
+        const synchronize = config.get<boolean>('db.sync') ?? false;
         if (!type) {
           throw new Error('Database type must be set');
         }
