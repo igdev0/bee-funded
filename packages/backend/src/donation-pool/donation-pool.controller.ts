db3726aa319f33ffac5d8430767f455c8478a35c
igdev <dorultanianos@gmail.com>
2025-08-10 10:53:41 +0300

    feat(donation-pool): create update endpoint

--- packages/backend/src/donation-pool/donation-pool.controller.ts
+++ packages/backend/src/donation-pool/donation-pool.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import CreateDonationPoolDto from './dto/create-donation-pool.dto';
 import { DonationPoolService } from './donation-pool.service';
+import UpdateDonationPoolDto from './dto/update-donation-pool.dto';
+import { GetUser } from '../user/user.decorator';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Controller('donation-pool')
 export class DonationPoolController {
   constructor(private readonly donationPoolService: DonationPoolService) {}
+
   @Post()
   create(@Body() body: CreateDonationPoolDto) {
     return this.donationPoolService.create(body);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @GetUser() user: UserEntity,
+    @Body() body: UpdateDonationPoolDto,
+  ) {
+    return this.donationPoolService.update(id, user.profile.id, body);
   }
 }
