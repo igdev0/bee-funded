4217b6c3e41543075d840baccafc2bdaa4c226ba
igdev <dorultanianos@gmail.com>
2025-08-10 11:16:24 +0300

    feat(donation-pool): protect create and update endpoints

--- packages/backend/src/donation-pool/donation-pool.controller.ts
+++ packages/backend/src/donation-pool/donation-pool.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import CreateDonationPoolDto from './dto/create-donation-pool.dto';
 import { DonationPoolService } from './donation-pool.service';
 import UpdateDonationPoolDto from './dto/update-donation-pool.dto';
 import { GetUser } from '../user/user.decorator';
 import { UserEntity } from '../user/entities/user.entity';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('donation-pool')
 export class DonationPoolController {
   constructor(private readonly donationPoolService: DonationPoolService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() body: CreateDonationPoolDto) {
     return this.donationPoolService.create(body);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
