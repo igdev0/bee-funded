bba080b3d89fc7ccea2a14783e3ddc346aee21ef
igdev <dorultanianos@gmail.com>
2025-08-10 10:51:41 +0300

    feat(donation-pool): add donation pool create endpointg

--- packages/backend/src/donation-pool/donation-pool.controller.ts
+++ packages/backend/src/donation-pool/donation-pool.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import CreateDonationPoolDto from './dto/create-donation-pool.dto';
+import { DonationPoolService } from './donation-pool.service';
 
 @Controller('donation-pool')
-export class DonationPoolController {}
+export class DonationPoolController {
+  constructor(private readonly donationPoolService: DonationPoolService) {}
+  @Post()
+  create(@Body() body: CreateDonationPoolDto) {
+    return this.donationPoolService.create(body);
+  }
+}
