ebacb006cbc6c753bd54cd7fab5ea8b0155fca2d
igdev <dorultanianos@gmail.com>
2025-08-11 19:04:27 +0300

    feat(donation-pool): add update image endpoint

--- packages/backend/src/donation-pool/donation-pool.controller.ts
+++ packages/backend/src/donation-pool/donation-pool.controller.ts
@@ -5,7 +5,9 @@ import {
   Param,
   Patch,
   Post,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import CreateDonationPoolDto from './dto/create-donation-pool.dto';
 import { DonationPoolService } from './donation-pool.service';
@@ -13,6 +15,7 @@ import UpdateDonationPoolDto from './dto/update-donation-pool.dto';
 import { GetUser } from '../user/user.decorator';
 import { UserEntity } from '../user/entities/user.entity';
 import { AuthGuard } from '../auth/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('donation-pool')
 export class DonationPoolController {
@@ -28,6 +31,19 @@ export class DonationPoolController {
   @Get(':id')
   get(@GetUser() user: UserEntity, @Param('id') id: string) {
     return this.donationPoolService.getOwned(id, user.profile.id);
+  }
+
+  @Patch(':id/update-avatar')
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor('image'))
+  updateImage(
+    @Param('id') id: string,
+    @GetUser() user: UserEntity,
+    @UploadedFile() uploadedFile: Express.Multer.File,
+  ) {
+    return this.donationPoolService.update(id, user.profile.id, {
+      image: `${uploadedFile.destination}/${uploadedFile.filename}`,
+    });
   }
 
   @UseGuards(AuthGuard)
