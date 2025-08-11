8084ca669c7ca7f4ee87af71f9446f1156b14bdf
igdev <dorultanianos@gmail.com>
2025-08-11 15:40:03 +0300

    chore(database): update database name

--- packages/backend/src/database/database.config.ts
+++ packages/backend/src/database/database.config.ts
@@ -6,7 +6,7 @@ export default registerAs('db', () => {
     type: process.env.DB_TYPE ?? 'postgres',
     host: process.env.DB_HOST ?? 'localhost',
     port: process.env.DB_PORT ?? 5432,
-    database: process.env.DB_DATABASE ?? 'example',
+    database: process.env.DB_DATABASE ?? 'bee-funded',
     username: process.env.DB_USERNAME ?? 'admin',
     password: process.env.DB_PASSWORD ?? 'admin',
     sync: true,
