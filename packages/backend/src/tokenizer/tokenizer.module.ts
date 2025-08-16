73da2d14503a4fc6a43abd4936dfbc40d188d979
igdev <dorultanianos@gmail.com>
2025-08-16 10:17:46 +0300

    chore(tokenizer): formatting

--- packages/backend/src/tokenizer/tokenizer.module.ts
+++ packages/backend/src/tokenizer/tokenizer.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { TokenizerService } from './tokenizer.service';
 
 @Module({
-  providers: [TokenizerService]
+  providers: [TokenizerService],
 })
 export class TokenizerModule {}
