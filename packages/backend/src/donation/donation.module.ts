66c94c269697c885d5899f03fb7203b04790a978
igdev <dorultanianos@gmail.com>
2025-08-18 15:25:21 +0300

    feat(donation): add required typeorm module import

--- packages/backend/src/donation/donation.module.ts
+++ packages/backend/src/donation/donation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DonationService } from './donation.service';
 import { DonationController } from './donation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DonationEntity from './entity/donation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DonationEntity])],
   providers: [DonationService],
   controllers: [DonationController],
 })
