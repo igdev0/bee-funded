3f651158cb5d46c5b68a2822353f8bdef32bc8d6
igdev <dorultanianos@gmail.com>
2025-08-29 21:57:47 +0300

    feat(donation): add get many by pool id endpoint

--- packages/backend/src/donation/donation.controller.ts
+++ packages/backend/src/donation/donation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { GetUser } from '../user/user.decorator';
 import { UserEntity } from '../user/entities/user.entity';
@@ -16,5 +16,14 @@ export class DonationController {
     @GetUser() user: UserEntity,
   ) {
     return this.donationService.getManyOwned(user.profile.id, page, limit);
+  }
+
+  @Get('by-pool/:poolId')
+  getByPool(
+    @Param('poolId') poolId: string,
+    @Query('limit') limit: number,
+    @Query('page') page: number,
+  ) {
+    return this.donationService.getManyByPoolId(poolId, limit, page);
   }
 }
