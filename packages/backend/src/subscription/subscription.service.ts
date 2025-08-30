ad61f16a5b130b53ad991a3afcf4455516c7323f
igdev <dorultanianos@gmail.com>
2025-08-30 15:38:13 +0300

    feat(subscription): import services within subscription serv

--- packages/backend/src/subscription/subscription.service.ts
+++ packages/backend/src/subscription/subscription.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import SubscriptionEntity from './entities/subscription.entity';
+import { Repository } from 'typeorm';
+import { ProfileService } from '../profile/profile.service';
 
 @Injectable()
-export class SubscriptionService {}
+export class SubscriptionService implements OnModuleDestroy, OnModuleInit {
+  constructor(
+    @InjectRepository(SubscriptionEntity)
+    private readonly subscriptionRepository: Repository<SubscriptionEntity>,
+    private readonly profileService: ProfileService,
+  ) {}
+
+  onModuleInit(): any {}
+
+  onModuleDestroy(): any {}
+}
