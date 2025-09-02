4c1fad00d879fca1984f5744284290f189bb289b
igdev <dorultanianos@gmail.com>
2025-09-02 21:51:26 +0300

    fix(subscription): fix test failing

--- packages/backend/src/subscription/subscription.service.spec.ts
+++ packages/backend/src/subscription/subscription.service.spec.ts
@@ -1,11 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SubscriptionService } from './subscription.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {SubscriptionService} from './subscription.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {DatabaseModule} from '../database/database.module';
+import SubscriptionEntity from './entities/subscription.entity';
+import {AuthModule} from '../auth/auth.module';
+import {ProfileModule} from '../profile/profile.module';
+import {ChainModule} from '../chain/chain.module';
+import {NotificationModule} from '../notification/notification.module';
+import {MailModule} from '../mail/mail.module';
 
 describe('SubscriptionService', () => {
   let service: SubscriptionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, AuthModule, ProfileModule, ChainModule, NotificationModule, MailModule, TypeOrmModule.forFeature([SubscriptionEntity])],
       providers: [SubscriptionService],
     }).compile();
 
